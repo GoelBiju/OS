4bb7916762ef15c66ac1947c3a3891609eb03b34
GoelBiju <goelmolel@hotmail.com>
2019-07-21 21:42:52 +0100

    Added code to linker.ld and

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,17 @@
 
-void kernelMain(void* multiboot_structure, unsigned int magicnumber) 
+// Print a string to the screen in black and white.
+void printf(char* str) 
+{
+    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+    // Copy the string to the VideoMemory location.
+    for (int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+
+// Entry point to the kernel.
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) 
 {
     printf("Hello world!");
 
